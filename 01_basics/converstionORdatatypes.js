a9adc818798ba51f0bafc653c552034d2aa370f0
Anjali sharma <109501930+Anjalisharma3463@users.noreply.github.com>
2024-03-10 10:25:42 +0000

    conversion and operatons

---
+++ 01_basics/converstionORdatatypes.js
@@ -0,0 +1,33 @@
+// let score = 33
+// let scores = "33abc"
+
+// //kahi baar we dont know what data someone sent to us . is it string or number
+// // so do this
+// console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof scores);
+// console.log(typeof(scores));
+
+
+// let valueInNumber = Number(scores) keep capital N capital n
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+/* output is nan for "33abc"
+ and 33 for "33" and
+ 0 for null and nan for underfinded
+1 for true 
+ nan for "Anjali*/
+
+//ALL THESE CONVERSION LIKE BOOLEAR , NUMBER ETC. are datatypes
+
+//  let isloggedIn = "anjali"
+//  let bolleanIsloggedin = Boolean(isloggedIn) keep capital B capital b
+//  console.log(bolleanIsloggedin);
+         // true is for 1
+         // false for ""
+         // true for "anjali"
+
+         let somenumber = 33
+         let stringnumber = String(somenumber) // keep capital S capital s
+         console.log(stringnumber);
+         console.log(typeof(stringnumber))
