585e139b5d07c60d23cc9da7453b1cd3e7477f98
Anjali sharma <109501930+Anjalisharma3463@users.noreply.github.com>
2024-03-18 04:40:37 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,25 @@
+let val = 5
+let neg = -val;
+// console.log(neg);
+
+// console.log(2+2);
+// console.log(2**3); // 2  ki power 3
+
+let str1 = "anjali"
+let str2 = "hello"
+
+let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1" + 2 ); // 12 output
+// console.log("1" + 2 + 2); / /122
+// console.log(1 + 2 + " 1"); // 3 1 // guidelines of ECMAscript..
+// console.log(+true); // 1... code should be cleaned . not good praticse
+// console.log(true+); // error
+
+let num1 , num2 , num3
+num1 = num2 = num3 =  2 + 2 // should not be confusing code
+let gamecounter = 100
+// gamecounter++ // 101
+// ++gamecounter // 101                        //learn form mdn site
+// console.log(gamecounter);                        // get link from descipiton
+
