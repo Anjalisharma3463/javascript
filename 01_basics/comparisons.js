d29d355f8a60ff130a6779098b201b26d7667fe6
Anjali sharma <109501930+Anjalisharma3463@users.noreply.github.com>
2024-03-19 14:10:36 +0000

    comparisons

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2 != 1);
+
+
+// console.log("2"> 1); // true
+// console.log("02"> 1); //true
+// console.log("1"> 2); // false
+
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+// console.log(null > 0); // false
+
+// euqland lessthan all these signs work differnelty
+
+// console.log(undefined== 0); // false
+// console.log(undefined > 0); // fasle
+// console.log(undefined == 0); // false
+
+//=== // strict check
+// console.log("2" == 2); //true 
