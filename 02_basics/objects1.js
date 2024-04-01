2ea480538bc2ff2a8118ec904cd394d50963c444
Anjali sharma <109501930+Anjalisharma3463@users.noreply.github.com>
2024-04-01 21:37:29 +0000

    updated

--- 02_basics/objects1.js
+++ 02_basics/objects1.js
@@ -1,1 +1,53 @@
 // singleton
+// singleton
+Object.create //(concstuctor) 
+
+
+// object literals
+const mysym =Symbol("key1")
+
+const JsUser = {
+    name: "anjali",
+   mysym: "mykey1", // string type
+   [mysym]: "mykey1", //symbol type //it iwill show up in when you print object lke log(jsuser);
+    age: 18,
+    location: "jaipur",
+    email : "anjalisharma@google.com",
+    islogedin : false,
+    lastlogindays : ["monday", "sat"]
+
+}
+// console.log(JsUser.email); //1st
+// console.log(JsUser["email"]); // 2nd way~
+// console.log(JsUser["full name"]); //only one way using square notation
+// console.log(JsUser.mysym); // using symbol as key //mykey1
+// console.log(JsUser[mysym]); //myket . by iti s goot syntax
+// console.log( typeof(JsUser.mysym)); // string type
+// console.log(typeof(JsUser[mysym]));//string
+
+
+    JsUser.email =  "hatgpt@GamepadHapticActuator.com"
+    // Object.freeze(JsUser) // is line k baad no change n this object
+    JsUser.email = "anjaswadkjgldgld"
+    // console.log(JsUser);
+
+  JsUser.greeting = function(){
+    console.log("hello js user;");
+  }  
+  console.log(JsUser.greeting); // undefinded when it was freeze but then if not the n output is ]functon(anoynyjios0)]
+ //funtion is not eecuted but   
+//  //or
+  console.log(JsUser.greeting()); // not a function now for now coz t was a freezeobject js user
+
+
+  JsUser.greeting = function(){
+    console.log( `hello this is jus user, ${this.name}`);
+  }  
+
+
+  JsUser.greetingtwo = function(){
+    console.log("hello js user;");
+  }  
+
+  console.log(JsUser.greeting());
+  console.log(JsUser.greetingtwo());
